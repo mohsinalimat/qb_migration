6533b84cff8547e25344b2df591b7fb8887ef564
tushar-javaid <tusharjavaid99@gmail.com>
2026-07-18 12:27:17 +0500

    Added the logic to handle the qty in the file Purchase Orders

--- qb_migration/qb_migration/migration/importers/purchase_orders.py
+++ qb_migration/qb_migration/migration/importers/purchase_orders.py
@@ -65,8 +65,12 @@ class PurchaseOrderImporter(BaseImporter):
             qty = line.get("qty")
             if qty is None:
                 qty = 1
-            if qty == 0 or qty == 0.0:
+            if (qty == 0 or qty == 0.0) and not (line.get("price", 0) > 0 or line.get("ext_price", 0) > 0):
                 continue
+            if (qty == 0 or qty == 0.0) and line.get("qty_received", 0) > 0:
+                qty = line["qty_received"]
+            if (qty == 0 or qty == 0.0) and not line.get("qty_received", 0):
+                qty = 1
 
             items.append({
                 "idx": idx,
